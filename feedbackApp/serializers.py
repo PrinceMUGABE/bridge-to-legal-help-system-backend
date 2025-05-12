d42d7265b68325e549633f155d394fe4bb3e1748
MugabePrince <princemugabe568@gmail.com>
2025-05-12 13:59:02 +0300

    starting to work on feedbacks

---
+++ feedbackApp/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Feedback
+
+from userApp.models import CustomUser
+from relocationApp.serializers import RelocationSerializer
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'phone_number', 'email', 'role', 'created_at']
+
+class FeedbackSerializer(serializers.ModelSerializer):
+    
+    created_by = UserSerializer(read_only=True)
+    relocation = RelocationSerializer(read_only=True)
+    
+    class Meta:
+        model = Feedback
+        fields = "__all__"
